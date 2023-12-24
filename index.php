3de978b6d8413a1999af049fe6f9eaa0430f478b
Anas <anasfasseh3@gmail.com>
2023-12-24 18:07:00 +0100

    affichage de la liste des taches

--- index.php
+++ index.php
@@ -20,14 +20,32 @@
     <link rel="stylesheet" href="../bootstrap-5.3.2-dist/css/bootstrap.min.css">
 </head>
 <body>
-    <nav class="navbar bg-dark text-light">
+<nav class="navbar">
         TodoList
     </nav>
-
-    <form action="" method="post">
-        <input type="text" name="title" placeholder="Task Title">
-        <input type="submit" value="Add" class="btn btn-primary">
-    </form>
+    <div class="container">
+        <form method="post">
+            <div class="adding-form">
+                <input type="text" name="title" placeholder="Task Title">
+                <input type="submit" value="Add" class="btn" name="add">
+                
+            </div>
+            <div class="taches-list">
+            <?php  
+                $res = $c->query('SELECT * FROM todo ORDER BY created_at DESC');
+                    while($line = $res->fetch(PDO::FETCH_ASSOC)) {
+                        $id = $line['id'];
+                        if ($line['done'] == '0') {
+                            echo "<div class='not-done'><span>".$line['title']."</span><div><a href='terminer.php?id=$id'><input type='button' value='done' name='done' class='btn special-btn' /></a><a href='supprimer.php?id=$id'><input type='button' value='X' class='xbtn' /></a></div></div>";
+                        } else {
+                            echo "<div class='done'><span>".$line['title']."</span><div><a href='undo.php?id=$id'><input type='button' value='undo' class='btn special-btn' /></a><a href='supprimer.php?id=$id'><input type='button' value='X' class='xbtn' /></a></div></div>";
+                        }
+                        
+                    };
+            ?>
+            </div>
+        </form>
+    </div>
 
     <script src="../bootstrap-5.3.2-dist/js/bootstrap.min.js"></script>
 </body>
